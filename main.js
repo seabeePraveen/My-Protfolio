f9584f104942132abdcb3d734960c94bebc8aefa
praveen <cbsrirangapraveen.2004@gmail.com>
2023-05-31 16:54:40 +0530

    reply from contact form modifeid in javascript

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function getReplyForContactSubmit(){
-    var element = document.getElementById("submit-response");
-    element.textContent = "Thanks for contacing me! I will reply as soon as possible";
+function getReplyForContactSubmit(event){
+    event.preventDefault();
+
+    var input1 = document.getElementById("name").value;
+    var input2 = document.getElementById("email").value;
+    var input3 = document.getElementById("message").value;
+
+    if(input1!="" && input2!="" && input3!=""){
+        var element = document.getElementById("submit-response");
+        element.textContent = "Thanks for contacing me! I will reply as soon as possible";
+    }
+
 }
 
