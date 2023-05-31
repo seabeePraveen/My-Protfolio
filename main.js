510525a352056c083d86e99fd6ccfbdaf6ab1039
praveen <cbsrirangapraveen.2004@gmail.com>
2023-05-31 23:05:35 +0530

    footer also done

--- main.js
+++ main.js
@@ -4,11 +4,15 @@ function getReplyForContactSubmit(event){
     var input1 = document.getElementById("name").value;
     var input2 = document.getElementById("email").value;
     var input3 = document.getElementById("message").value;
+    console.log("first");
 
     if(input1!="" && input2!="" && input3!=""){
         var element = document.getElementById("submit-response");
         element.textContent = "Thanks for contacing me! I will reply as soon as possible";
+        console.log("done");
     }
 
 }
 
+const form = document.querySelector('#myForm');
+form.addEventListener('submit',getReplyForContactSubmit);
