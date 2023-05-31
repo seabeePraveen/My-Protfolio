833a8214a7d2df395ffcd8ad200def029773c027
praveen <cbsrirangapraveen.2004@gmail.com>
2023-05-31 16:38:10 +0530

    javascript added for reply to contact form

---
+++ main.js
@@ -0,0 +1,5 @@
+function getReplyForContactSubmit(){
+    var element = document.getElementById("submit-response");
+    element.textContent = "Thanks for contacing me! I will reply as soon as possible";
+}
+
