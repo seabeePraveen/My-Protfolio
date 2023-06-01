77526f2c5b54f7b511caa81a65ebe6771055b0c8
praveen <cbsrirangapraveen.2004@gmail.com>
2023-06-01 18:43:08 +0530

    form reply caption on submit done

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-console.log("hello");
 
 const navicon = document.getElementById('menu-bar-nav');
 
@@ -11,4 +10,17 @@ const wrongicon = document.getElementById('wrong-mark');
 wrongicon.addEventListener('click',function(){
     const navitems = document.getElementById('mobile-menu-div');
     navitems.classList.add('closed-menu');
+});
+
+const formsubmit = document.querySelector('#form-submit');
+
+formsubmit.addEventListener('click',function(){
+    
+    const input1 = document.getElementById('name');
+    const input2 = document.getElementById('email');
+
+    if(input1.value!="" && input2.value!=""){
+        const matter = document.getElementById('submit-response');
+        matter.textContent = "Thanks for contacting! I will reply as soon as possible";
+    }
 });
